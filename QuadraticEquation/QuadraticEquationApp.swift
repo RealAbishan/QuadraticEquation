cefa694930e27b220e6e896dc9adf8bc5273713d
Abishan <highoncode09@gmail.com>
2023-02-28 21:07:04 +0530

    Initial Commit

---
+++ QuadraticEquation/QuadraticEquationApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuadraticEquationApp.swift
+//  QuadraticEquation
+//
+//  Created by Abishan Parameswaran on 2023-02-28.
+//
+
+import SwiftUI
+
+@main
+struct QuadraticEquationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
